2d718b734877f29f53f936071f2f096400cef12b
Harjas Singh <harjassingh3373@gmail.com>
2025-09-28 13:38:51 +0530

    Add files via upload

---
+++ main.py
@@ -0,0 +1,21 @@
+# Task 1
+a1=float(input("Enter the first number:"))
+a2=float(input("Enter the second number:"))
+b=a1+a2
+b=int(b)
+print("Addition: ", b)
+c=a1-a2
+c=int(c)
+print("Subtration: ", c)
+d=a1*a2
+d=int(d)
+print("Multiplication: ", d)
+e=a1/a2
+e=int(e)
+print("Division: ",d)
+
+
+# Task 2 
+x=input("Enter your first name: ")
+y=input("Enter your last name:")
+print("Hello," , x + y, "! Welcome to the python program")
